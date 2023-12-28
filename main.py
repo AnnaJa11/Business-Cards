aee4dd1e71c2e87e67da38f869ed4b90c7be2872
Anna J <ajasion5@gmail.com>
2023-12-28 14:45:18 +0000

    add list my_cards with data

---
+++ main.py
@@ -0,0 +1,34 @@
+
+my_cards = [
+    {'first_name':'Laura', 
+     'last_name': 'Welch', 
+     'company': "Cosmopolitan", 
+     'occupation': "accountant", 
+     'email': 'laura.welch@test.com'},
+
+      {'first_name':'Leo', 
+     'last_name': 'Michaels', 
+     'company': "Impact Ltd", 
+     'occupation': "designer", 
+     'email': 'leo.michaels@test.com'},
+
+      {'first_name':'Naomi', 
+     'last_name': 'Norack', 
+     'company': "BMI Ltd", 
+     'occupation': "lawyer", 
+     'email': 'naomi.norack@test.com'},
+
+      {'first_name':'Jack', 
+     'last_name': 'Damon', 
+     'company': "Italian Style", 
+     'occupation': "Sales Manager", 
+     'email': 'jack.damon@test.com'},
+
+      {'first_name':'Peter', 
+     'last_name': 'Brown', 
+     'company': "ABD Consulting", 
+     'occupation': "Marketing Manager", 
+     'email': 'peter.brown@test.com'},
+
+]
+
